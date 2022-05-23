4da30db48ac908c0969145ffc85148fdab2a4e79
Elliot Garnero <elliot.garnero@altametris.com>
2022-05-23 15:00:57 +0200

    simplel anding page and navbar

--- front/src/component/parts/header.jsx
+++ front/src/component/parts/header.jsx
@@ -1,8 +1,39 @@
+import Button from 'react-bootstrap/Button';
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+
 const Header = () => {
     return (
-        <div>
-            header
-        </div>
+        <Navbar bg="light" expand="lg">
+        <Container fluid>
+            <Navbar.Brand>Swinger room</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+            <Nav
+                className="me-auto my-2 my-lg-0"
+                style={{ maxHeight: '100px' }}
+                navbarScroll>
+                <Nav.Link href="#">
+                    Login
+                </Nav.Link>
+                <Nav.Link href="#">
+                    Sigin
+                </Nav.Link>
+            </Nav>
+            <Form className="d-flex">
+                <FormControl
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"/>
+                <Button variant="primary">Search</Button>
+            </Form>
+            </Navbar.Collapse>
+        </Container>
+        </Navbar>
     )
 }
 
