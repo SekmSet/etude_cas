fead896d42fcc7a1f0f00b6953af4779f1f0976b
Elliot Garnero <elliot.garnero@altametris.com>
2022-05-24 10:20:34 +0200

    form login register OK

--- front/src/component/auth/register.jsx
+++ front/src/component/auth/register.jsx
@@ -20,25 +20,67 @@ const Register = () => {
         }
     };
 
+    const formContainer = {
+        width: "380px",
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        transform: "translate(-50%, -50%)",
+        backgroundColor: "rgba(248,249,250,.3)",
+        padding: "20px 50px 40px",
+        boxShadow: "1px 4px 10px 1px #aaa",
+        borderRadius: "10px",
+    }
+
+    const formTitle = {
+        textAlign: "center",
+        marginBottom: "30px",
+    }
+
+    const textInput = {
+        display: "block",
+        height: "32px",
+        padding: "6px 16px",
+        marginBottom: "30px",
+        width: "100%",
+        border: "none",
+        backgroundColor: "#f3f3f3",
+        borderRadius: "5px",
+    }
+
+    const confirmButton = {
+        display: "block",
+        margin: "20px auto 0",
+        width: "150px",
+        height: "40px",
+        borderRadius: "25px",
+        border: "none",
+        color: "#fff",
+        fontWeight: "500",
+        boxShadow: "1px 4px 10px 1px #aaa",
+        backgroundColor: "#0d6efd",
+    }
+
     return (
-        <div>
+        <div style={formContainer}>
+            <h2 style={formTitle}>S'enregistrer</h2>
             <form className="form_register" onSubmit={handleSubmit(handleRegister)}>
                 <div className="form_group">
                     <label>Adresse mail :</label>
-                    <input {...register("email")} />
+                    <input {...register("email")} style={textInput} />
                 </div>
 
                 <div className="form_group">
                     <label>Mot de passe</label>
-                    <input type="password" {...register("password")}  />
+                    <input type="password" {...register("password")} style={textInput} required/>
                 </div>
 
                 <div className="form_group">
                     <label>Je confirme mon mot de passe</label>
-                    <input type="password" {...register("confirmPassword")} />
+                    <input type="password" {...register("confirmPassword")} style={textInput} required/>
                 </div>
 
-                <input type="submit" className="button" value="Valider" />
+                <input type="submit" className="button" value="Valider" style={confirmButton} required/>
             </form>
         </div>
     )
