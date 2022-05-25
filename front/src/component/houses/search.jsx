ab260a4dc7fb8b5dd70ecd920e1b6953704e17ce
Elliot Garnero <elliot.garnero@altametris.com>
2022-05-25 10:39:59 +0200

    homepage simple no linking OK

--- front/src/component/houses/search.jsx
+++ front/src/component/houses/search.jsx
@@ -1,7 +1,25 @@
+const styles = {
+    mainContainer : {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        backgroundColor: "rgba(220, 220, 220, 0.7)",
+        width: "500px",
+    },
+    inputStyling : {
+        width: "300px",
+        margin: "20px",
+        padding: "5px",
+        borderRadius: "10px",
+    },
+}
+
 const Search = () => {
     return (
-        <div>
-            HOME
+        <div style={styles.mainContainer}>
+            <input type="text" style={styles.inputStyling} name="where" id="where" placeholder="Je pars où ?"/>
+            <label htmlFor="date">Je pars quand ?</label>
+            <input type="date" style={styles.inputStyling} name="date" id="date" />
         </div>
     )
 }
--- front/src/component/pages/home.jsx
+++ front/src/component/pages/home.jsx
@@ -1,8 +1,29 @@
+import Search from "../houses/search";
+
+const styles = {
+    mainContainer : {
+        marginTop: "50px",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    titleStyling : {
+        color: "rgb(220, 220, 220)"
+    },
+    searchStyling: {
+        marginTop: "50px",
+    }
+}
+
 const Home = () => {
     return (
-
-        <div>
-            Home
+        <div style={styles.mainContainer}>
+            <h1 style={styles.titleStyling}>Une envie, un voyage ...</h1>
+            <h2 style={styles.titleStyling}>Trouvez un logement partout en France pour vous accompagner !</h2>
+            <h2 style={styles.titleStyling}>Où que vous alliez nous sommes là, avec vous.</h2>
+            <div style={styles.searchStyling}>
+                <Search />
+            </div>
         </div>
     )
 }
